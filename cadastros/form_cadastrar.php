19f917e7fe0762208e852a80629db1135c772aa1
jessica06 <jessicastn17@hotmail.com>
2014-01-07 18:26:19 -0200

    Create form_cadastrar.php

---
+++ cadastros/form_cadastrar.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+            <title>Cadastro de Pessoas</title>
+    </head>    
+    <body>
+        <?php
+            require_once("menu.php");
+        ?>
+        <form action="cadastrar.php" method="post"> 
+           <input type="text" name="nome" /> 
+           <input type="submit" value="Enviar" />  
+        </form>    
+        
+        
+        
+    </body>
+</html>
