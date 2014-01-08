566ede0025458d130ca932fbc26bb3fe3810da01
jessica06 <jessicastn17@hotmail.com>
2014-01-08 15:15:26 -0200

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 9;
+$b = 12;
+
+
+$dia = 11;
+$mes = 12;
+$ano = 2013;
+$texto = "A data hoje é ";
+
+echo $texto . $dia . "/" . $mes . "/" . $ano;
+echo "<br/>";
+echo " O dia de hoje é $dia/$mes/$ano";
+echo "<br/>";
+echo "$a + $b = " . ($a + $b);
+
+
+?>
