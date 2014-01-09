58dcd63e3aeec0336c861ee15a01d45845b621d1
jessica06 <jessicastn17@hotmail.com>
2014-01-09 16:48:12 -0200

    Update form_cadastrar.php

--- cadastros/form_cadastrar.php
+++ cadastros/form_cadastrar.php
@@ -7,7 +7,7 @@
             require_once("menu.php");
         ?>
         <form action="cadastrar.php" method="post"> 
-           <input type="text" name="nome" /> 
+          <?php require_once ("campos_pessoa.php"); ?>
            <input type="submit" value="Enviar" />  
         </form>    
         
