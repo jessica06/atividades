a16f21aa22a5afc62f75b794e916a9ff18347f97
jessica06 <jessicastn17@hotmail.com>
2014-01-09 16:52:11 -0200

    Create campos_pessoa.php

---
+++ cadastros/campos_pessoa.php
@@ -0,0 +1,8 @@
+<label>Nome: </label>
+ <input type="text" name="nome" />
+ <br/>
+ <input type="radio" name="sexo" value="M" checked="checked" />
+ <label>Masculino</label>
+ <input type="radio" name="sexo" value="F" />
+ <label>Feminino</label>
+ <br/>
