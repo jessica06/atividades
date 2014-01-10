5e6117a673d67f354e62799fcb4468c91fe35bda
jessica06 <jessicastn17@hotmail.com>
2014-01-10 16:12:53 -0200

    Update campos_pessoa.php

--- cadastros/campos_pessoa.php
+++ cadastros/campos_pessoa.php
@@ -6,3 +6,16 @@
  <input type="radio" name="sexo" value="F" />
  <label>Feminino</label>
  <br/>
+ <input type="checkbox" name="aceito" value="sim" />
+ <label>Aceito Contrato</label>
+ <br/>
+ <label>Estado</label>
+ <select name="estado">
+    <option>Bahia</option>
+    <option>São Paulo</option>
+    <option>Rio de Janeiro</option>
+</select>
+ <br/>
+ <label>Observações</label>
+ <br/>
+ <textarea name="observacoes" rows="5" cols="20"></textarea>
