326dc2117341db52fbde054b4cb829f59a6dd267
jessica06 <jessicastn17@hotmail.com>
2014-01-10 16:13:23 -0200

    Update cadastrar.php

--- cadastros/cadastrar.php
+++ cadastros/cadastrar.php
@@ -1,3 +1,5 @@
+
+
 <?php
 require_once("menu.php");
 
@@ -7,12 +9,27 @@ require_once("menu.php");
         $_SESSION["cadastros"] = array ();
     }
     
+    /* Aqui é onde pega os dados do formulario*/
     $nome = $_REQUEST["nome"];
     $sexo = $_REQUEST["sexo"];
+    $estado = $_REQUEST["estado"];
+    $observacoes = $_REQUEST["observacoes"];
+    /*FIM - */
     
+    $aceito = false;
+    if(isset($_REQUEST["aceito"])){
+        $aceito = true;
+    }
+   
+    /* preencher o cadastro da pessoa , o nome ,sexo, estado etc..*/
     $pessoa = array();
     $pessoa ["nome"] = $nome;
     $pessoa ["sexo"] = $sexo;
+    $pessoa ["aceito"] = $aceito;
+    $pessoa ["estado"] = $estado;
+    $pessoa ["observacoes"] = $observacoes;
+    
+   /* array_push = coloca o cadastro novo no final da lista*/ 
     
     array_push($_SESSION["cadastros"], $pessoa);
 
@@ -20,4 +37,3 @@ require_once("menu.php");
 
 
 ?>
-
