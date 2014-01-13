ac82b84189c70c0405fb8fd6236da2283ab0e35f
jessica06 <jessicastn17@hotmail.com>
2014-01-13 19:18:32 -0200

    Create campos_produtos.php

---
+++ avaliacao_3/campos_produtos.php
@@ -0,0 +1,24 @@
+<label>Nome: </label>
+ <input type="text" name="nome" />
+ <br/><br/>
+ <input type="radio" name="cor" value="Rosa" checked="checked" />
+ <label>Rosa</label>
+ <input type="radio" name="cor" value="Lilás" />
+ <label>Lilás</label>
+ <input type="radio" name="cor" value="Azul" />
+ <label>Azul</label>
+ <br/>
+ <label>Prazo de Entrega</label>
+ <select name="prazo">
+    <option>5 Dias </option>
+    <option>10 Dias</option>
+    <option>15 Dias</option>
+</select>
+ <br/>
+ <label>Detalhes do Produto</label>
+ <br/>
+ <textarea name="detalhes" rows="5" cols="17"></textarea>
+<br/>
+ <input type="checkbox" name="produtonovo" value="sim" />
+ <label>Com Carreto:</label>
+ <br/>
